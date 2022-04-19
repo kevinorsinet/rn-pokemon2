a24699fe31cc7f17da4e3cef29f4d90b4727cc69
kevinorsinet <kevin.orsinet@gmail.com>
2022-04-19 10:19:01 +0200

    ajout fetch api pokemon

--- App.tsx
+++ App.tsx
@@ -1,12 +1,35 @@
+import React, { useEffect, useState } from 'react';
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, SafeAreaView } from 'react-native';
+import Header from './src/components/Header';
 
 export default function App() {
+
+  const [pokemons, setPokemons] = useState([])
+
+  useEffect(() => {
+    fetch('https://pokeapi.co/api/v2/pokemon/?limit=10',{
+      method: 'GET',
+      headers :{
+        'Accept': 'application/json'
+      }
+    })
+    .then(response =>response.json()) // Parse le JSON
+    .then(data => {
+        setPokemons(data.results) 
+    })
+  }, [])
+  // console.log("pokemons", pokemons);
+  
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={{marginTop: 30}}>
+      <Header />
+      <View>      
+        <Text>Liste des Pokémons</Text>
+      </View>
+
+    </SafeAreaView>
+
   );
 }
 
